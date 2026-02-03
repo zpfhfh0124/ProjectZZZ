f4099fcbdecbf1b7397621e73b8f08c465a02cfb
rottencarrot0 <qpwo3824@gmail.com>
2026-02-03 15:41:52 +0900

    체이싱 문제 수정

--- ProjectZZZ/Source/ProjectZZZ/ProjectZZZ.Build.cs
+++ ProjectZZZ/Source/ProjectZZZ/ProjectZZZ.Build.cs
@@ -10,7 +10,7 @@ public class ProjectZZZ : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GamePlayAbilities", "GamePlayTags", "GamePlayTasks"  });
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
